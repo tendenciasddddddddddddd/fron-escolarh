4fb7ff181880291131a29bf8abd6232ad713aed0
esthelapp <51924032+tendenciasddddddddddddd@users.noreply.github.com>
2022-01-22 01:09:00 -0500

    Add files via upload

---
+++ docs/precache-manifest.6ade77f4ac398ea1f66544823efd9b1f.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8c55b5f03c9f177559",
+    "url": "/fron-escolarh/css/app.68161344.css"
+  },
+  {
+    "revision": "1763c3fc63b05cd22151",
+    "url": "/fron-escolarh/css/chunk-vendors.a8161988.css"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/fron-escolarh/fonts/Avenir.b5ed9330.ttf"
+  },
+  {
+    "revision": "858e8ae6d87c456ccee6f55e11f62fa9",
+    "url": "/fron-escolarh/fonts/AvenirNext-Regular.858e8ae6.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fron-escolarh/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fron-escolarh/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fron-escolarh/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fron-escolarh/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "bc826ea7e9571bdd568514f3b2ca219a",
+    "url": "/fron-escolarh/img/88fdd.bc826ea7.svg"
+  },
+  {
+    "revision": "e8ddf89a2e3deae7d8a4d27836e3d4fe",
+    "url": "/fron-escolarh/img/CMS-Hub.e8ddf89a.svg"
+  },
+  {
+    "revision": "d02046c6217440742982fca3fc943c7a",
+    "url": "/fron-escolarh/img/Desktop.d02046c6.svg"
+  },
+  {
+    "revision": "34fc6409baea8db1be3ca12a721e6b8b",
+    "url": "/fron-escolarh/img/Mascot-CursosenGrupo.34fc6409.png"
+  },
+  {
+    "revision": "7e8a981210ca8f74c17cf93af6ebb9ef",
+    "url": "/fron-escolarh/img/Orange_Checkmark.7e8a9812.svg"
+  },
+  {
+    "revision": "707f6727ee62958f0e2b0fc771bdc03c",
+    "url": "/fron-escolarh/img/aaaa.707f6727.png"
+  },
+  {
+    "revision": "817fccf713c04e0d30964b8a606a2988",
+    "url": "/fron-escolarh/img/account-security.817fccf7.svg"
+  },
+  {
+    "revision": "0d95c8aa178562efe52aeab6c1f519a0",
+    "url": "/fron-escolarh/img/add-on-reporting.0d95c8aa.svg"
+  },
+  {
+    "revision": "0636f3dd4921ae4659f2d68d63281d81",
+    "url": "/fron-escolarh/img/add-on-transactional-email.0636f3dd.svg"
+  },
+  {
+    "revision": "96f17e1984022efdc235e0880bf20352",
+    "url": "/fron-escolarh/img/all.96f17e19.svg"
+  },
+  {
+    "revision": "04163fda1112f6557b9f856a527cf101",
+    "url": "/fron-escolarh/img/analytics.04163fda.svg"
+  },
+  {
+    "revision": "5624e79927d3422614f30e5a7efab80b",
+    "url": "/fron-escolarh/img/astronaut2.5624e799.svg"
+  },
+  {
+    "revision": "eb3e4a951dff403df3ed5707936a14d7",
+    "url": "/fron-escolarh/img/bannerAulas.eb3e4a95.jpg"
+  },
+  {
+    "revision": "494223e33384aa3adf2aa60b2a57ab70",
+    "url": "/fron-escolarh/img/bot-avatar.494223e3.webp"
+  },
+  {
+    "revision": "ed09410bdb91b2fcf5c71df9c9e83ae5",
+    "url": "/fron-escolarh/img/calendar.ed09410b.jpg"
+  },
+  {
+    "revision": "3443e2b39a1da0f6b237c2f5e0612ffd",
+    "url": "/fron-escolarh/img/conect.3443e2b3.svg"
+  },
+  {
+    "revision": "55cb6eec54172a263315ffb689c579ac",
+    "url": "/fron-escolarh/img/config.55cb6eec.png"
+  },
+  {
+    "revision": "5a510d335b3bc18e6e0afdbb147c827f",
+    "url": "/fron-escolarh/img/contacts.5a510d33.svg"
+  },
+  {
+    "revision": "30e695b6a4b3f2c92e391e1d1b49bcbd",
+    "url": "/fron-escolarh/img/criar-cursos.30e695b6.jpg"
+  },
+  {
+    "revision": "c190d96326a66d15aac6b2bb54d7e316",
+    "url": "/fron-escolarh/img/crm.c190d963.svg"
+  },
+  {
+    "revision": "a5c5ee3f056b7d245e5295cd16668556",
+    "url": "/fron-escolarh/img/cursos-online.a5c5ee3f.png"
+  },
+  {
+    "revision": "ee26b05433ac44ab0f0b9403dd5bbca3",
+    "url": "/fron-escolarh/img/deployments-feature.ee26b054.svg"
+  },
+  {
+    "revision": "b56e345fa1202648e2d020b583fb9eec",
+    "url": "/fron-escolarh/img/descanso.b56e345f.svg"
+  },
+  {
+    "revision": "841a7fc68ccff08707b6aef149d86f13",
+    "url": "/fron-escolarh/img/descarga.841a7fc6.svg"
+  },
+  {
+    "revision": "779b1afd586afa179eeb16502bd18f2c",
+    "url": "/fron-escolarh/img/documents.779b1afd.svg"
+  },
+  {
+    "revision": "3b01d9c7df29fb3ef7f715b78bab7075",
+    "url": "/fron-escolarh/img/el-gobierno-de-todo.3b01d9c7.jpg"
+  },
+  {
+    "revision": "a13e7120b8c1f26e8d9244eed77525d0",
+    "url": "/fron-escolarh/img/electronic-signature.a13e7120.svg"
+  },
+  {
+    "revision": "692a61ec23200fc9656e1f999b4535fd",
+    "url": "/fron-escolarh/img/escudo.692a61ec.png"
+  },
+  {
+    "revision": "4ebc968233742cdad75c32c859c3cde8",
+    "url": "/fron-escolarh/img/events.4ebc9682.svg"
+  },
+  {
+    "revision": "57cf6886255aa0a1304c6c307b0b3d17",
+    "url": "/fron-escolarh/img/general.57cf6886.svg"
+  },
+  {
+    "revision": "15c6fcb658f9f8406fbdcb76378f7f4b",
+    "url": "/fron-escolarh/img/grupo.15c6fcb6.svg"
+  },
+  {
+    "revision": "52d6e692e22c24fa7cca40dcb7e5d8ed",
+    "url": "/fron-escolarh/img/guidelines.52d6e692.svg"
+  },
+  {
+    "revision": "4a8cb4266380b79dbba25397d55e0caf",
+    "url": "/fron-escolarh/img/headers.4a8cb426.svg"
+  },
+  {
+    "revision": "1549bce8dca082237f12944ce93f9e2a",
+    "url": "/fron-escolarh/img/hoja.1549bce8.svg"
+  },
+  {
+    "revision": "bac1115139812fee471bf432d2fe8a6b",
+    "url": "/fron-escolarh/img/integrations.bac11151.svg"
+  },
+  {
+    "revision": "0c7cbab66180aba5d3b4abc9f6a69025",
+    "url": "/fron-escolarh/img/logo-ministerio-educacion.0c7cbab6.png"
+  },
+  {
+    "revision": "02aac94b4f405f419dd1f9b5ea005333",
+    "url": "/fron-escolarh/img/lupa.02aac94b.svg"
+  },
+  {
+    "revision": "01a09d4d0dc1e2998c34befc15424909",
+    "url": "/fron-escolarh/img/map.01a09d4d.svg"
+  },
+  {
+    "revision": "daa7dc06e16a404d5f5ab936a3046fbd",
+    "url": "/fron-escolarh/img/middle-blob.daa7dc06.svg"
+  },
+  {
+    "revision": "386bddb89280514def005c4ccee652dd",
+    "url": "/fron-escolarh/img/multiple-objects.386bddb8.svg"
+  },
+  {
+    "revision": "c59312275ac22a40d8142f9b814cba0a",
+    "url": "/fron-escolarh/img/nofound.c5931227.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/fron-escolarh/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "9a85752042ae270b88114895b82da2c1",
+    "url": "/fron-escolarh/img/pdf.9a857520.png"
+  },
+  {
+    "revision": "162a25040a72087a50f93ea2ca1f090a",
+    "url": "/fron-escolarh/img/playbooks.162a2504.svg"
+  },
+  {
+    "revision": "20576c79ecc974ea2b5cac6d6199d126",
+    "url": "/fron-escolarh/img/private.20576c79.svg"
+  },
+  {
+    "revision": "57514e9515ac2b7319b1ce4751f34e5f",
+    "url": "/fron-escolarh/img/profile.57514e95.jpg"
+  },
+  {
+    "revision": "fc9e92bd1be2fc441ea3a83deffd2067",
+    "url": "/fron-escolarh/img/search-error.fc9e92bd.svg"
+  },
+  {
+    "revision": "ea6604ecf4b0994add67c157cff3d9e5",
+    "url": "/fron-escolarh/img/segmented-lists.ea6604ec.svg"
+  },
+  {
+    "revision": "407e0bd9ab5aa06f48a4596f2f6e1c8a",
+    "url": "/fron-escolarh/img/sombra1.407e0bd9.svg"
+  },
+  {
+    "revision": "eaf757e688f2986977e8f72ff859e159",
+    "url": "/fron-escolarh/img/status_offline.eaf757e6.svg"
+  },
+  {
+    "revision": "c329eadb767040a1ac513d3a3700ede5",
+    "url": "/fron-escolarh/img/success-green.c329eadb.svg"
+  },
+  {
+    "revision": "3882ed83e27f80c6d9de4ea6869388dc",
+    "url": "/fron-escolarh/img/successful-send.3882ed83.svg"
+  },
+  {
+    "revision": "ef49fcf5fefc1f89d4ff9fd32da2e0c4",
+    "url": "/fron-escolarh/img/task.ef49fcf5.svg"
+  },
+  {
+    "revision": "1e894f8dfb75cae7cc672bba31b2761b",
+    "url": "/fron-escolarh/img/ticket.1e894f8d.svg"
+  },
+  {
+    "revision": "577644aac9055c1b86d181ca8705b770",
+    "url": "/fron-escolarh/img/training-crm.577644aa.svg"
+  },
+  {
+    "revision": "f914d7039fbd39af1b8d6dab197e7057",
+    "url": "/fron-escolarh/img/undraw_search.f914d703.svg"
+  },
+  {
+    "revision": "7357baafb62f0eab1f5d896cf06676c5",
+    "url": "/fron-escolarh/img/vacio.7357baaf.svg"
+  },
+  {
+    "revision": "d49f12f375218495d16d43e1afc156ad",
+    "url": "/fron-escolarh/img/zonas.d49f12f3.svg"
+  },
+  {
+    "revision": "f3b03871f4a9ffce808c11b71558d7ba",
+    "url": "/fron-escolarh/index.html"
+  },
+  {
+    "revision": "2f8c55b5f03c9f177559",
+    "url": "/fron-escolarh/js/app.f3e6052c.js"
+  },
+  {
+    "revision": "1763c3fc63b05cd22151",
+    "url": "/fron-escolarh/js/chunk-vendors.8869e8cd.js"
+  },
+  {
+    "revision": "d3eaceba3af4c050ca6933c18777cd51",
+    "url": "/fron-escolarh/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fron-escolarh/robots.txt"
+  },
+  {
+    "revision": "ee55a8c2a2093484ee91b9df973f7066",
+    "url": "/fron-escolarh/statics/js/core/bootstrap.min.js"
+  },
+  {
+    "revision": "50dbd6ab5b2feda47fb7d3b0d1db6bb2",
+    "url": "/fron-escolarh/statics/js/core/popper.min.js"
+  },
+  {
+    "revision": "21e771df8c43c1161520b9e64ed04ebe",
+    "url": "/fron-escolarh/statics/js/plugins/bootstrap-notify.js"
+  },
+  {
+    "revision": "9e815e576519269bbe1c3b5ec1620905",
+    "url": "/fron-escolarh/statics/js/plugins/multistep-form.js"
+  },
+  {
+    "revision": "7c3f4f11d3eefce9765d573f0e99edf9",
+    "url": "/fron-escolarh/statics/js/soft-ui-dashboard.js"
+  },
+  {
+    "revision": "a7c7f646048ed5a815f493d510f5cce0",
+    "url": "/fron-escolarh/statics/js/soft-ui-dashboard.min.js"
+  }
+]);
